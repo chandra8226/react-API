cb642135a1eb763ee5b5ebcbd3b2e311b3300433
Chandra Prakash <chandra8226@gmail.com>
2021-10-02 21:07:21 +0530

    fixed #2

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState(null)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const result = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(result.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, [loading])
+  if (loading === true) {
+    return (
+      <>
+        <section>
+          loading.....
+        </section>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        posts.map((post) => (
+          <div key={post.id}>
+            <div className="card text-center">
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text">{post.body}</p>
+              </div>
+              <div className="card-footer text-muted">
+                2 days ago
+              </div>
+            </div>
+            <br></br>
+          </div>
+        ))}
+
+
+
+
+
+    </>
   );
 }
 
